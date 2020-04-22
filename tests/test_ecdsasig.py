fc384625a57934f26835e479f7bd1c268a5b12ce
Eric Ehlers <eric.ehlers@reposit.be>
2020-04-22 14:39:13 +0200

    implement verification of signatures in the test scripts

--- tests/test_ecdsasig.py
+++ tests/test_ecdsasig.py
@@ -2,8 +2,10 @@
 
 import binascii
 
-from ledgerblue.ecWrapper import PrivateKey
+from ledgerblue.ecWrapper import PrivateKey, PublicKey
 from ledgerblue.comm import getDongle
+
+PUBLIC_KEY="046F760E57383E3B5900F7C23B78A424E74BEBBE9B7B46316DA7C0B4B9C2C9301C0C076310EDA30506141DD47C2D0A8A1D7CA2542482926AE23B781546193B9616"
 
 message = "76955a821a8aa04acb48203a7df7113bfd1a0f479552d9098e032fa9b1917018"
 
@@ -16,6 +18,10 @@ apdu = bytearray.fromhex(apduMessage)
 apdu.extend(messageBin)
 
 dongle = getDongle(True)
-ret = dongle.exchange(apdu)
-print("sig=\n" + str(binascii.hexlify(ret)))
+signature = dongle.exchange(apdu)
+print("signature=\n" + str(binascii.hexlify(signature)))
+
+publicKey = PublicKey(binascii.unhexlify(PUBLIC_KEY), True)
+verified = publicKey.ecdsa_verify(messageBin, signature, True)
+print("verified=" + str(verified))
 
--- tests/test_ecdsasig_offline.py
+++ tests/test_ecdsasig_offline.py
@@ -2,15 +2,22 @@
 
 import binascii
 
-from ledgerblue.ecWrapper import PrivateKey
+from ledgerblue.ecWrapper import PrivateKey, PublicKey
 
-PRIVATE_KEY="cf1094c960a753e8f848411aaae578a7083391165c4d39b78af6e81c310d0b5e"
+PUBLIC_KEY="046F760E57383E3B5900F7C23B78A424E74BEBBE9B7B46316DA7C0B4B9C2C9301C0C076310EDA30506141DD47C2D0A8A1D7CA2542482926AE23B781546193B9616"
+PRIVATE_KEY="B0F212DD8651A02AB7E2EA38A718A2547B04EFB4E31C9A42D5676BF41378D9E3"
 
-message = "745f30a9bf9a90a7d529a6bbba9635762baea83da2e2f147e5738b9795d22b85"
+message = "76955a821a8aa04acb48203a7df7113bfd1a0f479552d9098e032fa9b1917018"
 
 print("message=" + message)
 
-masterPrivate = PrivateKey(binascii.unhexlify(PRIVATE_KEY))
-signature = masterPrivate.ecdsa_sign(binascii.unhexlify(message), raw=True)
+messageBin = binascii.unhexlify(message)
+
+privateKey = PrivateKey(binascii.unhexlify(PRIVATE_KEY))
+signature = privateKey.ecdsa_sign(messageBin, raw=True)
 print("signature=" + str(binascii.hexlify(signature)))
+
+publicKey = PublicKey(binascii.unhexlify(PUBLIC_KEY), True)
+verified = publicKey.ecdsa_verify(messageBin, signature, True)
+print("verified=" + str(verified))
 
