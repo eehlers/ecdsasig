bde4cfc72ed00b42476ecce9a10cbc7b5f94bbde
Eric Ehlers <eric.ehlers@reposit.be>
2020-06-15 11:12:17 +0200

    WIP

--- tests/test_ecdsasig.py
+++ tests/test_ecdsasig.py
@@ -4,14 +4,24 @@ from ledgerblue.ecWrapper import PrivateKey, PublicKey
 from ledgerblue.comm import getDongle
 from ledgerblue.loadApp import parse_bip32_path
 
-#PUBLIC_KEY="0480F8889656CE6CCACBAD707D16E9805BA05E160D48066FEBC98FC28E785B46FEA57C854096EA0ED65434283148D84E39567BB379E214ABC8539A25E6605A5882"
+# This test assumes that your ledger has been initialized with the seed below,
+# which comes from the ledger emulator,
+# speculos.py in repo https://github.com/LedgerHQ/speculos.git.
+# If you use a different seed then you will need to modify the test accordingly.
+#DEFAULT_SEED = 'glory promote mansion idle axis finger extra february uncover one trip resource lawn turtle enact monster seven myth punch hobby comfort wild raise skin'
+
+TEST_PATH = "42'/0'"
+
+# The public key that will be used to verify the signature that comes back from the ledger.
+# This key corresponds to DEFAULT_SEED and TEST_PATH.
+# If you change the seed and/or path then you will need a different key to verify the signature.
 PUBLIC_KEY="0499C3F628A6BCDEDCCC4AD6213EBD672A484EB7AE657FE6A8FA05D52AC9ED047C0D856564FD9C75E6FFF0DE70387B9B5AA94675B327078C18A3ED2E1486A4DDCC"
 
-message = "76955a821a8aa04acb48203a7df7113bfd1a0f479552d9098e032fa9b1917018"
+MESSAGE = "76955a821a8aa04acb48203a7df7113bfd1a0f479552d9098e032fa9b1917018"
 
-print("message=" + message)
+print("MESSAGE=" + MESSAGE)
 
-messageBin = bytes.fromhex(message)
+messageBin = bytes.fromhex(MESSAGE)
 
 apduMessage = "E003"
 apdu = bytearray.fromhex(apduMessage)
